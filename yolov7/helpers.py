ba905c7b2b992a5c5b57f3c42b79a94206698e45
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-07-27 16:03:15 +0530

    Update helpers.py

--- yolov7/helpers.py
+++ yolov7/helpers.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from yolov7.models.common import AutoShape, DetectMultiBackend
+#from yolov7.models.common import AutoShape, DetectMultiBackend
+from yolov7.models.experimental import attempt_load
 from yolov7.utils.general import LOGGER, logging
 from yolov7.utils.torch_utils import torch
 
