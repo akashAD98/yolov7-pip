186b7f3d5caed7a3a41db276133e4040a3633c0f
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-07-28 11:01:49 +0530

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 class TestConstants:
     YOLOV7_MODEL_PATH = "yolov7/weights/yolov7.pt"
-    YOLOV7x_MODEL_PATH = "yolov7x/weights/yolov7x.pt"
+    YOLOv7_W6_MODEL_PATH = "yolov7/weights/yolov7-w6.pt"
     ZIDANE_IMAGE_PATH = "tests/data/zidane.jpg"
     BUS_IMAGE_PATH = "tests/data/bus.jpg"
