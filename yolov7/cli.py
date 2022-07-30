4c13ab9248f57fac8dc2c970c6ed3a8f39fc6b7a
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-07-30 09:24:37 +0530

    Update cli.py

--- yolov7/cli.py
+++ yolov7/cli.py
@@ -1,9 +1,9 @@
 import fire
 
-from yolov7-main.detect import run as detect
-from yolov7-main.models.common import export as export
-from yolov7-main.train import run_cli as train
-from yolov7-main.test import run as test
+from yolov7.detect import run as detect
+from yolov7.models.common import export as export
+from yolov7.train import run_cli as train
+from yolov7.test import run as test
 
 
 def app() -> None:
